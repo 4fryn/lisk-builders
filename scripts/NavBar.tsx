d6ed16b206471781a538fc4975aa7f9c3603793c
4fryn <4fryn@users.noreply.github.com>
2018-10-05 14:05:48 +0200

    scripts: simplify navbar

--- scripts/NavBar.tsx
+++ scripts/NavBar.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const title = '{ lisk.builders }';
+const title = '{ alternate.vote.manager }';
 export default class NavBar extends Component<any, any> {
 
   menu: any;
@@ -47,26 +47,14 @@ export default class NavBar extends Component<any, any> {
             <div className="column col-12">
               <header className="navbar">
                 <section className="navbar-section">
-                  <Link to="/" className="navbar-brand mr-10">{ title }</Link>
+                  <Link to="/votemanager" className="navbar-brand mr-10">{ title }</Link>
                 </section>
                 <section className="navbar-section">
-                  <div className="hide-md">
-                    <Link to="/" className="btn btn-link">Builders</Link>
-                    <Link to="/contributions" className="btn btn-link">Contributions</Link>
-                    <Link to="/votemanager" className="btn btn-link">Vote Manager</Link>
-                  </div>
                   <div className="show-md">
                     <div className="dropdown dropdown-right active">
                       <a href="#menu" className="btn btn-link" tabIndex={0} onClick={this.handleClick} ref={el => this.menu = el}>
                         Menu <i className="icon icon-caret" />
                       </a>
-                      { dropDownOpen &&
-                          <ul className="menu">
-                            <li><Link to="/" className="btn btn-link">Builders</Link></li>
-                            <li><Link to="/contributions" className="btn btn-link">Contributions</Link></li>
-                            <li><Link to="/votemanager" className="btn btn-link">Vote Manager</Link></li>
-                          </ul>
-                      }
                     </div>
                   </div>
                 </section>
